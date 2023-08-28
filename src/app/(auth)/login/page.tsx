83740071b3b3b0bf17e70b871f6fe4aeef82014f
Saga-sanga <vulcan248@gmail.com>
2023-08-28 21:56:47 +0530

    feat: add back link button

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,26 @@
 import { Icons } from "@/components/icons";
 import LoginForm from "@/components/login-form";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 
 export default function Page() {
   return (
-    <main className="space-y-6 min-h-screen grid place-content-center">
+    <main className="relative space-y-6 min-h-screen grid place-content-center">
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute top-4 left-4 md:top-8 md:left-8"
+        )}
+      >
+        <ChevronLeft className="mr-2 w-4 h-4"/>
+        Back
+      </Link>
       <div className="flex flex-col space-y-2 items-center text-center">
-        <Icons.logo className="w-12 h-12"/>
-        <h1 className="text-2xl font-semibold tracking-tight">
-          Welcome back
-        </h1>
+        <Icons.logo className="w-12 h-12" />
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
         <p className="text-sm text-muted-foreground">
           Enter your email below to login
         </p>
