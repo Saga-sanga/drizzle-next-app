019ecddbadd8a207e57c2160760ff4a53319e3e1
Saga-sanga <vulcan248@gmail.com>
2023-08-30 11:53:21 +0530

    feat: add google provider

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,14 +1,25 @@
 import { NextAuthOptions } from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { db } from "@/server";
 
 export const authOptions: NextAuthOptions = {
   adapter: DrizzleAdapter(db),
+  // session: {
+  //   strategy: "jwt",
+  // },
+  pages: {
+    signIn: "/login",
+  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!
-    })
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
   ],
 };
--- src/components/oauth-buttons.tsx
+++ src/components/oauth-buttons.tsx
@@ -5,10 +5,12 @@ import { Button } from "./ui/button";
 import { signIn } from "next-auth/react";
 
 export default function OAuthButtons({ isLoading }: { isLoading: boolean }) {
+  const handleLogin = () => signIn("github")
+
   return (
     <div className="flex flex-col gap-3">
       <Button
-        onClick={() => signIn("github", { callbackUrl: "/" })}
+        onClick={handleLogin}
         disabled={isLoading}
         size="lg"
         variant="outline"
@@ -20,7 +22,7 @@ export default function OAuthButtons({ isLoading }: { isLoading: boolean }) {
         )}
         Github
       </Button>
-      <Button disabled={isLoading} size="lg" variant="outline">
+      <Button onClick={() => signIn("google")} disabled={isLoading} size="lg" variant="outline">
         {isLoading ? (
           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
         ) : (
